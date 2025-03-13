844523dd48ac23bf072cd310992dc7d4cec1b37f
minirov1208 <a0939897580@gmail.com>
2025-03-13 16:58:17 +0800

    feat: add homepage page

---
+++ Vue_frontend/src/components/Navbar/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar">
+    <router-link to="/">🏠 首頁</router-link>
+    |
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: #333;
+  padding: 10px;
+  color: white;
+  text-align: center;
+  z-index: 1000; /* 確保在最上層 */
+}
+
+.navbar a {
+  color: white;
+  text-decoration: none;
+  margin: 0 10px;
+}
+
+.navbar a:hover {
+  text-decoration: underline;
+}
+
+/* 讓頁面內容不會被 Navbar 擋住 */
+.page-content {
+  margin-top: 50px; /* 視 Navbar 高度調整 */
+}
+</style>
---
+++ Vue_frontend/src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>歡迎來到 MyShop</h1>
+    <p>這是首頁</p>
+  </div>
+</template>
